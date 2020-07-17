0e8c3248eb8811989b5c0ffb7e5e4bee9d182dff
antony-guthiga <toniguthiga93@gmail.com>
2020-07-17 08:29:55 +0300

    delete upload image

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_simplemde import SimpleMDE
 
 bootstrap = Bootstrap()
@@ -11,7 +10,6 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-photos = UploadSet('photos',IMAGES)
 simple = SimpleMDE()
 
 def create_app(config_name):
@@ -33,7 +31,5 @@ def create_app(config_name):
     simple.init_app(app)
     login_manager.init_app(app)
 
-    # configure UploadSet
-    configure_uploads(app,photos)
 
     return app
