a1523cba3b6221c4c263e68dc4a971949591ce23
antony-guthiga <toniguthiga93@gmail.com>
2020-07-20 14:03:38 +0300

    add alternate database uri

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moring:Access@localhost/post' or SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'postgresql+psycopg2://moring:Access@localhost/post'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = '\xd49\xaaO\xd6\xed\xccJ\xf2\x84\xaaH\xe7NF\xd6=\x9dz\xc3\xc4\xc3\xf3\xc7'
 
